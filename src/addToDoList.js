fa2a9bd3108da550f069857c634e387294b811d6
joshiee02 <jkhirakosocmed@gmail.com>
2023-04-16 13:02:37 +0900

    add test javascript for header

---
+++ src/addToDoList.js
@@ -0,0 +1,4 @@
+export default function addTasks() {
+  console.log('it works');
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import addTasks from './addToDoList';
+
+const header = document.querySelector('header');
+
+header.addEventListener('click', () => {
+  addTasks();
+});
