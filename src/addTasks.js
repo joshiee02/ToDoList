03f5bc3103fa343c2c6b09c42955371484578405
joshiee02 <jkhirakosocmed@gmail.com>
2023-04-19 20:41:35 +0900

    add animation for form when header is clicked

--- src/addTasks.js
+++ src/addTasks.js
@@ -1,4 +1,17 @@
-export default function addTasks() {
-  console.log('it works');
-}
+export default function showForm() {
+  // adds classes to elements affected when form is active
+  const header = document.querySelector('header');
+  header.id = 'modal';
+  const h1 = document.querySelector('h1');
+  h1.id = 'modal_h1';
+  const img = document.querySelector('.mouse_cursor');
+  img.classList.add('modal_cursor');
 
+  setTimeout(() => {
+    const form = document.querySelector('form');
+    form.classList.remove('hidden');;
+    setTimeout(function() {
+      form.classList.add('show')
+    }, 100);
+  }, 300);
+}
