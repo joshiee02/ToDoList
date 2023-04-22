0a98fd457b824d6459a73e7f584694a7d20c927d
joshiee02 <jkhirakosocmed@gmail.com>
2023-04-22 10:33:49 +0900

    import addTask func in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './style.css';
-import showForm from './addTasks';
+import { showForm, addTask } from './addTasks';
 
 const header = document.querySelector('header');
-
 header.addEventListener('click', () => {
   showForm();
+});
+
+const submitButton = document.querySelector('button[type="submit"]');
+submitButton.addEventListener('click', () => {
+  addTask();
 });
