bd86156779bd0b62af7544669a884678f40d49a8
joshiee02 <jkhirakosocmed@gmail.com>
2023-04-23 10:58:03 +0900

    add functionality to store tasks locally

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ header.addEventListener('click', () => {
   showForm();
 });
 
-let task;
 const submitButton = document.querySelector('button[type="submit"]');
 submitButton.addEventListener('click', () => {
-  //avoids submit button to refresh
+  // avoids submit button to refresh
   event.preventDefault();
 
-  task = getTask();
-  console.log(task);
-  addTask(task.taskName, task.description, task.dueDate, task.taskPriority);
+  // gets tasks and inserts them in the taskContainer
+  const userTask = getTask();
+  addTask(userTask.taskName, userTask.description, userTask.dueDate, userTask.taskPriority);
+
+  const existingTasks = JSON.parse(localStorage.getItem('all-tasks')) || [];
+  existingTasks.push(userTask);
+  localStorage.setItem('all-tasks', JSON.stringify(existingTasks));
+  console.log(JSON.parse(localStorage.getItem('all-tasks')));
 });
